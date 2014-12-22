462b3df62c3d49d3b4f4a8ac1dc84f7c075e9b42
Artem Kolesnikov <sirruf@me.com>
2014-12-22 05:19:29 +0300

    Install Generator updated.

--- lib/generators/clean_web/install_generator.rb
+++ lib/generators/clean_web/install_generator.rb
@@ -1,9 +1,10 @@
 module CleanWeb
-  class InstallGenerator < Rails::Generators::Base
-    source_root File.expand_path('../../templates', __FILE__)
-    def create_config_file
-      return unless options.empty?
-      template 'clean_web.rb', 'config/initializers/clean_web.rb'
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../templates', __FILE__)
+      def create_config_file
+        template 'clean_web.rb', 'config/initializers/clean_web.rb'
+      end
     end
   end
 end
